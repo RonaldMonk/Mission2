d74aff60b43a877f14ccccdcfc6015652497d9cc
RonaldMonk <ronaldm@missionreadyhq.com>
2025-09-01 22:01:56 +1200

    Added comments and did some tidying up

--- script.js
+++ script.js
@@ -2,55 +2,47 @@ const constituencyAndList = [ // Array for each region
   {
     "region": "Central Scotland",
     "constSeats": [9, 0, 0, 0, 0, 0], //Constituency seats won by each party in the order SNP, Alba, Green, Labour, Conservative and Liberal democrats
-    "listVotes": [148399, 5345, 19512, 77623, 59896, 6337], //regional list votes
-    "altVotes": [0, 79544.5, 93711.5, 77623, 59896, 6337] // SNP votes given to Alba and Greens 50/50. I might not have time for this.
+    "listVotes": [148399, 5345, 19512, 77623, 59896, 6337] //regional list votes in the same order as constituency seats
   },
   {
     "region": "Glasgow",
     "constSeats": [9, 0, 0, 0, 0, 0],
-    "listVotes": [147091, 7820, 50364, 70942, 33758, 9523],
-    "altVotes": [0, 81365.5, 123909.5, 70942, 33758, 9523]
+    "listVotes": [147091, 7820, 50364, 70942, 33758, 9523]
   },
   {
     "region": "Highlands and Islands",
     "constSeats": [6, 0, 0, 0, 0, 2],
-    "listVotes": [96433, 3828, 6788, 22713, 60779, 26771],
-    "altVotes": [0, 52044.5, 55004.5, 22713, 60779, 26771]
+    "listVotes": [96433, 3828, 6788, 22713, 60779, 26771]
   },
   {
     "region": "Lothian",
     "constSeats": [7, 0, 0, 1, 0, 1],
-    "listVotes": [163022, 14973, 43936, 80518, 50678, 27160],
-    "altVotes": [0, 96484, 125447, 80518, 50678, 27160]
+    "listVotes": [163022, 14973, 43936, 80518, 50678, 27160]
   },
   {
     "region": "Mid Scotland and Fife",
     "constSeats": [8, 0, 0, 0, 0, 1],
-    "listVotes": [121094, 7064, 29996, 59077, 75061, 19911],
-    "altVotes": [0, 67611, 90543, 59077, 75061, 19911]
+    "listVotes": [121094, 7064, 29996, 59077, 75061, 19911]
   },
   {
     "region": "North East Scotland",
     "constSeats": [9, 0, 0, 0, 1, 0],
-    "listVotes": [158145, 10235, 34565, 69861, 80787, 24025],
-    "altVotes": [0, 89307.5, 113617.5, 69861, 80787, 24025]
+    "listVotes": [158145, 10235, 34565, 69861, 80787, 24025]
   },
   {
     "region": "South Scotland",
     "constSeats": [6, 0, 0, 0, 3, 0],
-    "listVotes": [133526, 8572, 28931, 60185, 84028, 22529],
-    "altVotes": [0, 75335, 95694, 60185, 84028, 22529]
+    "listVotes": [133526, 8572, 28931, 60185, 84028, 22529]
   },
   {
     "region": "West Scotland",
     "constSeats": [8, 0, 0, 1, 1, 0],
-    "listVotes": [147689, 10914, 28281, 71024, 63173, 19465],
-    "altVotes": [0, 84758.5, 102125.5, 71024, 63173, 19465]
+    "listVotes": [147689, 10914, 28281, 71024, 63173, 19465]
   }
 ];
 
 function createVoteTable(voteArray, seatsVotes){ //Create a table to display votes per region
-let txt="<tr><th>Region</th><th>SNP</th><th>Alba</th><th>Green</th><th>Labour</th><th>Cons</th><th>Libdem</th></tr>"; // Headings
+let txt="<tr><th>Region</th><th>SNP</th><th>Alba</th><th>Green</th><th>Labour</th><th>Cons</th><th>Libdem</th></tr>"; // Headings for first row
   for (const region of voteArray) { // for each region...
     txt +=`<tr><td>${region["region"]}</td>`; // Make a table entry for the name of the region
     for (const voteList of region[seatsVotes]) { // for each party...
@@ -66,39 +58,40 @@ document.getElementById("const-seats").innerHTML = createVoteTable(constituencyA
 document.getElementById("list-seats").innerHTML = createVoteTable(constituencyAndList, "listVotes"); // display regional list votes
 
 const parties = ["SNP", "Alba", "Green", "Labour", "Cons", "Libdem"];
-const centralListVotes = constituencyAndList[0].listVotes; //copy regional list votes for Central Scotland into a new array
-const listVotesWon = [0, 0, 0, 0, 0, 0];
-const centralConstVotes = constituencyAndList[0].constSeats; // copy constituency seat count into a new array
+const centralListVotes = constituencyAndList[0].listVotes; //copy regional list votes for Central Scotland into a less wordy new array
+const listSeatsWon = [0, 0, 0, 0, 0, 0];
+const centralConstVotes = constituencyAndList[0].constSeats; // copy constituency seat count into a less wordy new array
 const line3Desc = "List seats won";
 
-let tableLine2 = "<td>Constituency Seats</td>"; //set up content of table for D'Hondt demonstration
-for (const i of constituencyAndList[0].constSeats) {
+let tableLine2 = "<td>Constituency Seats</td>"; //set up content of table for D'Hondt demonstration (line 1 is in html)
+for (const i of centralConstVotes) {
   tableLine2 += `<td>${i}</td>`;// set up table data entries for constituency results
 }
 tableLine2 += "<td></td>";
-document.getElementById("const-won").innerHTML = tableLine2; // fixed content so this can be displayed just once
-let tableLine4 = "<td>Votes cast</td>"; // construct
-for (const k of constituencyAndList[0].listVotes) {
-  tableLine4 += `<td>${k}`;
+document.getElementById("const-won").innerHTML = tableLine2; // fixed content so this can be inserted into the table just once
+let tableLine4 = "<td>List votes cast</td>"; // start constructing 4th line of the table
+for (const k of centralListVotes) {
+  tableLine4 += `<td>${k}`; // add each cell to the table row
 };
-tableLine4 += "<td></td>";
-document.getElementById("votes-cast").innerHTML = tableLine4;
-let currentRound = 0;
+tableLine4 += "<td></td>"; // append an empty cell so that the border works properly
+document.getElementById("votes-cast").innerHTML = tableLine4; // insert row 4 which is fixed and doesn't have to be worried about any more
 
-function dHondtReset() {
+let currentRound; // used to keep track of which D'Hondt round we're on. No need to initialise as it will be done in reset function
+
+function dHondtReset() { // beginning and reset position of variables to be displayed and controlling variables (which round we're on)
   currentRound = 0;
-  for (const listArray in listVotesWon) {
-    listVotesWon[listArray] = 0;
+  for (const listArray in listSeatsWon) { // zero out all the regional list..should it be seats, not votes..that could be confusing
+    listSeatsWon[listArray] = 0;
   }
-  let tableLine3 = `<td>${line3Desc}</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td></td>` // reset list seats won to all blank
-  document.getElementById("list-won").innerHTML = tableLine3;
+  let tableLine3 = `<td>${line3Desc}</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td></td>` // reset list seats won to all zeros
+  document.getElementById("list-won").innerHTML = tableLine3; // append list seats line to table
   for (let j=1; j<=7; j++) {
     document.getElementById(`round${j}`).innerHTML =
-    `<td>-</td><td> </td><td> </td><td> </td><td> </td><td> </td><td> </td><td> </td>`; // clear rounds lines
+    `<td>-</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>`; // clear rounds lines
   }
-  document.getElementById("round-btn").innerHTML = "Start";
+  document.getElementById("round-btn").innerHTML = "Start"; // set button text for start of D'Hondt run
 }
-const dHondt = document.getElementById("round-btn");
+const dHondt = document.getElementById("round-btn"); // set up listener for Start/Next round/Restart button
 dHondt.addEventListener("click", nextDHondtRound);
 
 dHondtReset();
@@ -106,34 +99,34 @@ dHondtReset();
 function nextDHondtRound() {
   currentRound++;
   let dHontArray;
-  let text = `<td>Round ${currentRound}</td>`
+  let text = `<td>Round ${currentRound}</td>` // start setting up table row for D'Hondt round
   if (currentRound === 8) {
     dHondtReset();
   } else {
     dHontArray = centralListVotes.map(function(votes, inx){
-      return Math.round(votes/(centralConstVotes[inx]+listVotesWon[inx]+1));
+      return Math.round(votes/(centralConstVotes[inx]+listSeatsWon[inx]+1)); // apply the D'Hondt formula to all the party votes
     });
     let max = dHontArray[0];
     let maxInx = 0;
     for (let party in dHontArray) {
       if (dHontArray[party] >max) {
-        max = dHontArray[party];
-        maxInx = party;
+        max = dHontArray[party]; // Find the winning party
+        maxInx = party; // and save the index
       }
     };
     for (party of dHontArray) {
-      text += `<td>${party}</td>`
+      text += `<td>${party}</td>` // collect D'Hondt results into a string
     }
-    text += `<td style="text-align: left">${parties[maxInx]}</td>`
-    document.getElementById(`round${currentRound}`).innerHTML = text;
-    text = `<td>${line3Desc}</td>`;
-    listVotesWon[maxInx]++;
-    for (party of listVotesWon) {
-      text += `<td>${party}</td>`
+    text += `<td style="text-align: left">${parties[maxInx]}</td>` // override style to make winning party string left aligned
+    document.getElementById(`round${currentRound}`).innerHTML = text; // and append to the table
+    text = `<td>${line3Desc}</td>`; // set up row for updated regional list seats
+    listSeatsWon[maxInx]++;
+    for (party of listSeatsWon) {
+      text += `<td>${party}</td>` // collect party seats won into a string
     };
-    text += "<td></td>";
+    text += "<td></td>"; // add a cell to the end of the string
     document.getElementById("list-won").innerHTML = text;
-    document.getElementById("round-btn").innerHTML = (currentRound === 7)? "Reset": "Next";
+    document.getElementById("round-btn").innerHTML = (currentRound === 7)? "Reset": "Next round"; // If on round 7 changes button text
   }
 
 }
